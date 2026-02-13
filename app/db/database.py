a2cb0b2f19ec80bbabc5985b085d7315fe5210ca
Fxhat <make772@ukr.net>
2026-02-13 13:42:20 +0200

    fix flake8 errors & fix path to schema.sql

--- app/db/database.py
+++ app/db/database.py
@@ -6,8 +6,9 @@ from config import settings
 async def create_pool():
     return await asyncpg.create_pool(settings.DATABASE_URL)
 
+
 async def init_db(pool):
-    with open("app/db/schema.sql") as t:
+    with open("db/schema.sql") as t:
         schema = t.read()
     async with pool.acquire() as conn:
         await conn.execute(schema)
